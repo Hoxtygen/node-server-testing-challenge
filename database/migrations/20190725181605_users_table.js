99d2e29e986f705d21776cd5442d39dbe2f4136c
Hoxtygen <hoxtygen@live.com>
2019-07-25 18:26:48 +0100

    Set up database configuration

---
+++ database/migrations/20190725181605_users_table.js
@@ -0,0 +1,22 @@
+
+/* eslint-disable func-names */
+
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+
+    table.string('email')
+      .notNullable()
+      .unique();
+
+    table.text('password')
+      .notNullable();
+
+    table.string('department')
+      .notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users');
+};
